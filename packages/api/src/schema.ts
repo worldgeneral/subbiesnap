09c11c4bf1a1f0cb8aac591eeb0b802d27bc040d
Matt Harrison <matt@mymailhost.co.uk>
2024-03-06 12:33:05 +0000

    users table created

--- packages/api/src/schema.ts
+++ packages/api/src/schema.ts
@@ -1,3 +1,17 @@
-import { mysqlTable, bigint, varchar, date } from "drizzle-orm/mysql-core";
+import {
+  int,
+  mysqlTable,
+  varchar,
+  timestamp,
+  unique,
+} from "drizzle-orm/mysql-core";
 
-export const users = mysqlTable("users", {});
+export const users = mysqlTable("users", {
+  id: int("id").primaryKey().autoincrement(),
+  createdAt: timestamp("created_at", { mode: "date", fsp: 6 }),
+  updatedAt: timestamp("updated_at", { mode: "date", fsp: 6 }),
+  password: varchar("password", { length: 255 }),
+  email: varchar("email", { length: 255 }).unique(),
+  firstName: varchar("first_name", { length: 255 }),
+  secondName: varchar("second_name", { length: 255 }),
+});
