606ba71609f1dd51b334c31c29f192bc855dbbe3
Matt Harrison <matt@mymailhost.co.uk>
2024-03-07 21:30:14 +0000

    changed schema path

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./src/schema.ts",
+  schema: "./src/models/users.ts",
   out: "./drizzle",
   driver: "mysql2", // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
   dbCredentials: {
