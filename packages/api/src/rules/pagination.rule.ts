67a813270cd47348749e55fb8a094df4ebea9d72
Matt Harrison <matt@mymailhost.co.uk>
2024-04-21 10:58:43 +0100

    fix min/max number on pagination

--- packages/api/src/rules/pagination.rule.ts
+++ packages/api/src/rules/pagination.rule.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const paginationSchema = z.object({
-  limit: z.coerce.number(),
-  offset: z.coerce.number(),
+  limit: z.coerce.number().positive().lte(100),
+  offset: z.coerce.number().positive(),
 });
