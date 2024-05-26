e403ca06a1de2ebf2256a3b98a824270efa4edaf
Matt Harrison <matt@mymailhost.co.uk>
2024-05-26 21:21:17 +0100

    refactor: hard coded no reply email

--- packages/api/src/constants/emails.ts
+++ packages/api/src/constants/emails.ts
@@ -1,6 +1,4 @@
-import { COMPANY_NAME } from "./branding";
-
-export const NO_REPLY_EMAIL = `no-reply@${COMPANY_NAME}.com`;
+export const NO_REPLY_EMAIL = `no-reply@subbiesnap.com`;
 
 export const CONFIRM_EMAIL_ID_LENGTH = 30;
 
