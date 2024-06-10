f306c3e7b1959ecc2fcf90e16fc942d7430cc21b
matt harrison <matt@mymailhost.co.uk>
2024-06-10 21:41:57 +0100

    Feat: header with nav bar

---
+++ packages/app/src/app/components/header/Header.tsx
@@ -0,0 +1,19 @@
+import NavBar from "./navBar"
+export default function Header(){
+    return(
+    <>
+
+        <div>
+            <div>
+            <img src="" alt="SubbieSnap logo" />
+            </div>
+            <div>
+                <button>signup</button>
+                <button>login</button>
+            </div>
+
+        </div>
+        <NavBar/>
+    </>
+    )
+}
---
+++ packages/app/src/app/components/header/NavBar.tsx
@@ -0,0 +1,10 @@
+export default function NavBar() {
+    return (
+        <nav>
+           <button >home</button>
+           <button>jobs</button>
+           <button>companies</button>
+           <button>contractors</button>
+        </nav>
+    )
+}
