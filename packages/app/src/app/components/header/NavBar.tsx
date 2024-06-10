f0265bea92b69f77e8d8c4fc756e3c2096c21517
matt harrison <matt@mymailhost.co.uk>
2024-06-10 21:46:54 +0100

    feat: links in nav bar

--- packages/app/src/app/components/header/NavBar.tsx
+++ packages/app/src/app/components/header/NavBar.tsx
@@ -1,10 +1,11 @@
 export default function NavBar() {
     return (
         <nav>
-           <button >home</button>
-           <button>jobs</button>
-           <button>companies</button>
-           <button>contractors</button>
+           <a href="/">Home</a>
+           <a href="/jobs">Jobs</a>
+           <a href="/companies">Companies</a>
+           <a href="/contractors">Contractors</a>
+           <a href="/about">About Us</a>
         </nav>
     )
 }
