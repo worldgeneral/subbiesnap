4159aa74703592edf180c4214939b0e8e3eae948
worldgeneral <147430042+worldgeneral@users.noreply.github.com>
2024-06-11 20:05:31 +0100

    feat: header with navbar (#47)

---
+++ packages/app/src/components/header/header.tsx
@@ -0,0 +1,20 @@
+import NavBar from "./navBar"
+
+export default function Header(){
+    return(
+    <>
+
+        <div>
+            <div>
+            <img src="" alt="SubbieSnap logo" />
+            </div>
+            <div>
+                <button>signup</button>
+                <button>login</button>
+            </div>
+
+        </div>
+        <NavBar/>
+    </>
+    )
+}
---
+++ packages/app/src/components/header/navBar.tsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+    return (
+        <nav>
+           <a href="/">Home</a>
+           <a href="/jobs">Jobs</a> 
+           <a href="/companies">Companies</a>
+           <a href="/contractors">Contractors</a>
+           <a href="/about">About Us</a> 
+        </nav>
+    )
+}
