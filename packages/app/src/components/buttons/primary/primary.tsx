dd8880d135c613db041266554286a34115e31db1
matt harrison <matt@mymailhost.co.uk>
2024-06-18 21:45:45 +0100

    chore: button changes

--- packages/app/src/components/buttons/primary/primary.tsx
+++ packages/app/src/components/buttons/primary/primary.tsx
@@ -9,7 +9,7 @@ type props = {
 
 export function PrimaryButton(props: props) {
   const buttonClasses =
-    "p-4 h-10 text-white shadow-inner shadow-md bg-gradient-to-br from-button-primary-one from-50% to-button-primary-two to-50% rounded-3xl border-t-2 border-t-button-primary-two inline-block ";
+    " h-10 text-white shadow-inner shadow-md bg-gradient-to-br from-button-primary-one from-50% to-button-primary-two to-50% rounded-3xl border-t-2 border-t-button-primary-two inline-block ";
   return (
     <Link
       className={
@@ -18,7 +18,7 @@ export function PrimaryButton(props: props) {
       href={props.link}
     >
       <div className="flex items-center h-full">
-        <div className="basis-9/12 text-center">{props.buttonText}</div>
+        <div className="basis-9/12 mx-3 text-center">{props.buttonText}</div>
         <div className="border-2 rounded-full bg-button-primary-one border-slate-800 shadow-inner shadow-sm">
           <ArrowRightIcon />
         </div>
--- packages/app/src/components/buttons/secondary/secondary.tsx
+++ packages/app/src/components/buttons/secondary/secondary.tsx
@@ -10,7 +10,7 @@ type props = {
 
 export function SecondaryButton(props: props) {
   const buttonClasses =
-    "w-32 h-10 text-white shadow-inner shadow-md bg-gradient-to-br from-button-secondary-one from-50% to-button-secondary-two to-50% rounded-3xl border-t-2 border-t-indigo-500 inline-block ";
+    " h-10 text-white shadow-inner shadow-md bg-gradient-to-br from-button-secondary-one from-50% to-button-secondary-two to-50% rounded-3xl border-t-2 border-t-indigo-500 inline-block ";
   return (
     <Link
       className={
@@ -19,7 +19,7 @@ export function SecondaryButton(props: props) {
       href={props.link}
     >
       <div className="flex items-center h-full">
-        <div className="basis-9/12 text-center ">{props.buttonText}</div>
+        <div className="basis-9/12 mx-3 text-center ">{props.buttonText}</div>
         <div className="border-2 rounded-full border-indigo-500 bg-button-secondary-one shadow-inner shadow-sm">
           <ArrowRightIcon />
         </div>
